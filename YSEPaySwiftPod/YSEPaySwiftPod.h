7e2bbf852c6bfc7232f6144bfd1ea06ea3a59ded
spec <spectatornan@gmail.com>
2019-05-23 11:16:25 +0800

    Initial commit

---
+++ YSEPaySwiftPod/YSEPaySwiftPod.h
@@ -0,0 +1,19 @@
+//
+//  YSEPaySwiftPod.h
+//  YSEPaySwiftPod
+//
+//  Created by ios-spec on 2019/5/23.
+//  Copyright © 2019 mhly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSEPaySwiftPod.
+FOUNDATION_EXPORT double YSEPaySwiftPodVersionNumber;
+
+//! Project version string for YSEPaySwiftPod.
+FOUNDATION_EXPORT const unsigned char YSEPaySwiftPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSEPaySwiftPod/PublicHeader.h>
+
+
